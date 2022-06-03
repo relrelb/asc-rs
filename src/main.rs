d68579ceef1bd926cadf6f97ce89634ecc0fc882
relrelb <relrelbachar@gmail.com>
2022-06-03 23:44:45 +0300

    Improve CLI
    * Print usage when no arguments are given, instead of panicking.
    * Print an error when source file cannot be read, instead of panicking.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
+use std::borrow::Cow;
+
 mod compiler;
 mod scanner;
 
+fn usage() {
+    let program = std::env::args()
+        .nth(0)
+        .map_or(Cow::Borrowed("asc"), Cow::Owned);
+    println!("Usage: {} <file.as>", program);
+}
+
 fn main() {
-    let filename = std::env::args().nth(1).expect("missing filename");
-    let source = std::fs::read_to_string(&filename).expect("read failed");
+    let filename = match std::env::args().nth(1) {
+        Some(filename) => filename,
+        None => {
+            usage();
+            return;
+        }
+    };
+
+    let source = match std::fs::read_to_string(&filename) {
+        Ok(source) => source,
+        Err(error) => {
+            println!("Cannot read {}: {}", filename, error);
+            return;
+        }
+    };
+
     if let Err(error) = compiler::compile(&source) {
         let line = source.lines().nth(error.line - 1).unwrap();
         println!(
