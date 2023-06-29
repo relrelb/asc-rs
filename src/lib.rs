a05ebb1ee8dfb30cc5e472e13c5c7c10b2c6bf29
relrelb <relrelb@users.noreply.github.com>
2023-06-30 01:00:34 +0300

    Return error from main

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+mod compiler;
+mod scanner;
+
+pub use compiler::compile;
+pub use scanner::CompileError;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-mod compiler;
-mod scanner;
+use asc::CompileError;
 
 fn usage() {
     let program = std::env::args()
@@ -8,26 +7,22 @@ fn usage() {
     println!("Usage: {} <file.as>", program);
 }
 
-fn main() {
-    let filename = match std::env::args().nth(1) {
-        Some(filename) => filename,
-        None => {
-            usage();
-            return;
-        }
+fn main() -> Result<(), CompileError> {
+    let Some(filename) = std::env::args().nth(1) else {
+        usage();
+        return Ok(());
     };
 
-    let source = match std::fs::read_to_string(&filename) {
-        Ok(source) => source,
-        Err(error) => {
-            println!("Cannot read {}: {}", filename, error);
-            return;
-        }
-    };
+    let source = std::fs::read_to_string(&filename).map_err(|error| CompileError {
+        message: format!("Cannot read {}: {}", filename, error),
+        line: 0,
+        column: 0,
+    })?;
 
     let file = std::fs::File::create("test.swf").unwrap();
     let writer = std::io::BufWriter::new(file);
-    if let Err(error) = compiler::compile(&source, writer) {
+    let result = asc::compile(&source, writer);
+    if let Err(error) = &result {
         let line = source.lines().nth(error.line - 1).unwrap();
         println!(
             "{}:{}:{}: {}:\n\t{}\n\t{}^",
@@ -39,4 +34,5 @@ fn main() {
             " ".repeat(error.column - 1)
         );
     }
+    result
 }
